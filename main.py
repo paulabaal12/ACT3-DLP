5afba57c6ff9ef4f27c8eb5adf2e56a844c67c02
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2025-01-21 18:03:11 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Código base para iniciar
+def cargar_buffer(entrada, inicio, tamano_buffer):
+  buffer = entrada[inicio:inicio + tamano_buffer]
+  if len(buffer) < tamano_buffer:
+    buffer.append("eof")
+  return buffer
+
+def procesar_buffer(buffer):
+  # Procesar y extraer lexemas del buffer
+  pass
+
+entrada = list("Esto es un ejemplo eof")
+inicio = 0
+tamano_buffer = 10
+buffer = cargar_buffer(entrada, inicio, tamano_buffer)
+procesar_buffer(buffer)
